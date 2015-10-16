7245946822f26ca047ea6a5ee753ec19e27f79c7
BRKurek <bkurek2@illinois.edu>
2015-10-16 12:20:08 -0500

    Added more action bar functionality

---
+++ ProjectDarkZone/Assets/Scripts/ActionBarItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionBarItem
+{
+    public string name { get; set; }
+    public int amount { get; set; }
+
+    public ActionBarItem(string name, int amount)
+    {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public ActionBarItem()
+    {
+
+    }
+}
