f0db055a7e9f5ce4437741ae29dc21e44fb623ad
tagboy07 <tagboy07@gmail.com>
2015-11-01 16:24:02 -0600

    Overworld v0.5

---
+++ ProjectDarkZone/Assets/Scripts/ChestTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestTrigger : MonoBehaviour {
+
+	private bool open;
+	public Texture2D fadeOutTexture;
+
+	// Use this for initialization
+	void Start () {
+		open = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open && Input.GetKeyDown("e")) {
+			StartCoroutine(openChest());
+		}
+	}
+
+	IEnumerator openChest(){
+		GameObject.Find ("Fader").GetComponent<Fading> ().BeginFade (1);
+		yield return new WaitForSeconds (1.5f);
+		GameObject.Find ("Fader").GetComponent<Fading> ().BeginFade (-1);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Player") {
+			open = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.name == "Player") {
+			open = false;
+		}
+	}
+}
