cfcaad9cd3c0a946646651d78ef15a2ab65089be
Kody Puebla <kpuebla@imsa.edu>
2015-11-08 13:18:02 -0600

    Functional Main Menu

---
+++ ProjectDarkZone/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button play, exit;
+    
+	void Start () {
+        quitMenu.enabled = false;
+	}
+	
+	public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        play.enabled = false;
+        exit.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        play.enabled = true;
+        exit.enabled = true;
+    }
+
+    public void PlayGame()
+    {
+        Application.LoadLevel("Overworld");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
