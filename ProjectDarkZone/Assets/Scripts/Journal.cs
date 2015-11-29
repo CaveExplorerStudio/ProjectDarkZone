8f851f1e03b9aaf9d432db69dd803f6569e31016
Chris Cygnus <gamer00@att.net>
2015-11-29 17:19:47 -0600

    Added journal file

---
+++ ProjectDarkZone/Assets/Scripts/Journal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Journal : IItem {
+
+    public string Name { get; set; }
+    public Sprite Image { get; set; }
+    public bool IsConsumable { get; set; }
+    public GameObject Prefab { get; set; }
+
+    public Journal(string name, Sprite image, bool isConsumable, GameObject prefab)
+    {
+        Name = name;
+        Image = image;
+        IsConsumable = IsConsumable;
+        Prefab = prefab;
+    }
+
+    public void Use()
+    {
+
+    }
+}
