33662884c0e0ec1b995b6ec14fb1aef1cf0525a4
tagboy07 <tagboy07@gmail.com>
2015-11-29 17:18:01 -0600

    Close to being done overworld

---
+++ ProjectDarkZone/Assets/Scripts/keyPrompt.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keyPrompt : MonoBehaviour {
+
+	public string levelToLoad;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			StartCoroutine (StartGame ());
+		}
+	}
+
+	IEnumerator StartGame(){
+		yield return new WaitForSeconds (.1f);
+		Application.LoadLevel (levelToLoad);
+	}
+}
---
+++ ProjectDarkZone/Assets/Scripts/panel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class panel : MonoBehaviour {
+
+	private bool open;
+
+	// Use this for initialization
+	void Start () {
+		open = false;
+		gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(open) {
+			gameObject.SetActive(true);
+		}
+		if (!open) {
+			gameObject.SetActive (false);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Player") {
+			open = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.name == "Player") {
+			open = false;
+		}
+	}
+}
