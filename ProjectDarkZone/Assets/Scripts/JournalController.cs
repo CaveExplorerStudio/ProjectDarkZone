afeb5a32429cb4f0412ee38b5bee5bde6b3ab03a
christophercolumbusdog <gamer00@att.net>
2015-11-30 10:57:15 -0600

    Working journal fade

--- ProjectDarkZone/Assets/Scripts/JournalController.cs
+++ ProjectDarkZone/Assets/Scripts/JournalController.cs
@@ -11,6 +11,9 @@ public class JournalController : MonoBehaviour {
     private GameObject journalGUI;
 
     private bool journalActive;
+    private bool isFading;
+    private int fadeState;
+    private int fadeCounter = 10;
 
     void Start () {
         collectedPages = new List<journalPage>();
@@ -24,6 +27,51 @@ public class JournalController : MonoBehaviour {
 	
 	
 	void Update () {
-	
-	}
+	    if (Input.GetKeyDown(KeyCode.J) && !journalActive)
+        {
+            journalActive = !journalActive;
+            journalCanvas.enabled = true;
+            isFading = true;
+            fadeState = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.J) && journalActive)
+        {
+            journalActive = !journalActive;
+            isFading = true;
+            fadeState = 1;
+        }
+
+
+        if (isFading)
+            FadePage(fadeState);
+
+    }
+
+    private void FadePage(int i)
+    {
+        if (fadeCounter != 0 && i == 1)
+        {
+            fadeCounter--;
+            journalCanvasGroup.alpha -= .1f;
+        }
+        else if (i == 1)
+        {
+            isFading = false;
+            journalCanvas.enabled = false;
+            fadeCounter = 10;
+            journalCanvasGroup.alpha = 0;
+        }
+
+        if (fadeCounter != 0 && i == 0)
+        {
+            fadeCounter--;
+            journalCanvasGroup.alpha += .1f;
+        }
+        else if (i == 0)
+        {
+            isFading = false;
+            fadeCounter = 10;
+            journalCanvasGroup.alpha = 1;
+        }
+    }
 }
